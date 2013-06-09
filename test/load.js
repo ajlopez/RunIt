da9aa2850f100caaec1542dbd7d2619524e1a1f7
ajlopez <webmaster@ajlopez.com>
2013-06-09 10:01:31 -0300

    Load local runit module index function

--- test/load.js
+++ test/load.js
@@ -7,7 +7,7 @@ exports['Load function defined'] = function (test) {
     test.done();
 };
 
-exports['Load local runit module'] = function (test) {
+exports['Load local runit module file'] = function (test) {
     var setglobal = require('../runit/test/setglobal.js');
     var result = runit.load('test','setglobal');
 
@@ -16,3 +16,11 @@ exports['Load local runit module'] = function (test) {
     test.done();
 };
 
+exports['Load local runit module function'] = function (test) {
+    var setrunit = require('../runit/test/index.js').setrunit;
+    var result = runit.load('test','setrunit');
+
+    test.ok(result);
+    test.equal(result, setrunit);
+    test.done();
+};
