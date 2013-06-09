67c3aa941a352e77fd94eac5a1e87b900789d6a8
ajlopez <webmaster@ajlopez.com>
2013-06-09 09:54:33 -0300

    Run local runit test setglobal

--- runit/test/setglobal.js
+++ runit/test/setglobal.js
@@ -1,3 +1,4 @@
 
 module.exports = function (args, runit, options, cb) {
+    global.model[args[0]] = args[1];
 };
--- test/run.js
+++ test/run.js
@@ -6,3 +6,13 @@ exports['Run function defined'] = function (test) {
     test.equal(typeof runit.run, "function");
     test.done();
 };
+
+exports['Run local runit test setglobal'] = function (test) {
+    global.model = { };
+    
+    runit.run('test', 'setglobal', ['name', 'Adam']);
+    
+    test.ok(global.model.name);
+    test.equal(global.model.name, 'Adam');
+    test.done();
+};
